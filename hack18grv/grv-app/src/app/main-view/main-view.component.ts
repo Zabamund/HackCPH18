cfc70bf182d2892e2f5b106d35ac2aaaa9f85a16
Zabamund <fracgeol@gmail.com>
2018-06-09 11:24:05 +0200

    adding slider logic

--- hack18grv/grv-app/src/app/main-view/main-view.component.ts
+++ hack18grv/grv-app/src/app/main-view/main-view.component.ts
@@ -1,11 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'grv-main-view',
   templateUrl: './main-view.component.html',
-  styleUrls: ['./main-view.component.css']
+  styleUrls: ['./main-view.component.css'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class MainViewComponent implements OnInit {
+  autoTicks = false;
+  disabled = false;
+  invert = false;
+  max = 100;
+  min = 0;
+  showTicks = true;
+  step = 1;
+  thumbLabel = true;
+  value = 0;
+  vertical = false;
+
+  get tickInterval(): number | 'auto' {
+    return this.showTicks ? (this.autoTicks ? 'auto' : this._tickInterval) : 0;
+  }
+  set tickInterval(v) {
+    this._tickInterval = Number(v);
+  }
+  private _tickInterval = 10;
 
   constructor() { }
 
