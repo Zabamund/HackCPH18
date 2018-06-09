da33029ad21df2fdf3a9c3b215bce7b0a51702d6
Zabamund <fracgeol@gmail.com>
2018-06-09 09:00:09 +0200

    add NoopAnimationsModule for ng material

--- hack18grv/src/app/app.module.ts
+++ hack18grv/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +9,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NoopAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
