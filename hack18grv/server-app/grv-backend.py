d321ba01807a769d27e649a4b9849ed5dbf1e21d
Zabamund <fracgeol@gmail.com>
2018-06-09 14:42:08 +0200

    imports for python code

--- hack18grv/server-app/grv-backend.py
+++ hack18grv/server-app/grv-backend.py
@@ -2,9 +2,14 @@ from flask import Flask, request, jsonify
 from flask_restful import Resource, Api
 from flask_cors import CORS, cross_origin
 
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
 app = Flask(__name__)
-CORS(app)
 api = Api(app)
+CORS(app)
 
 @app.route('/')
 def home():
@@ -14,11 +19,12 @@ def home():
 def helloWorld():
   return "Hello, cross-origin-world!"
 
-class Employees(Resource):
-    def get(self):
-        return {'employees': [{'id':1, 'name':'Balram'},{'id':2, 'name':'Tom'}]} 
-
-api.add_resource(Employees, '/employees') # Route_1
+@app.route('/API', methods = ['GET'])
+def apicall():
+    if request.method == 'GET':
+        dictionary = {'var1':request.args.get('arg1'),
+                    'var2':request.args.get('arg2')}
+    return jsonify(dictionary)
 
 if __name__ == '__main__':
-    app.run(port=5002)
+    app.run(port=5002, debug=True)
