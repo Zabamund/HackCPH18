cbbe4075929d9c869d59f7096adef1bbeb63776f
Zabamund <fracgeol@gmail.com>
2018-06-09 13:33:27 +0200

    add CORS workaround

--- hack18grv/server-app/grv-backend.py
+++ hack18grv/server-app/grv-backend.py
@@ -1,12 +1,18 @@
 from flask import Flask, request, jsonify
 from flask_restful import Resource, Api
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
+CORS(app)
 api = Api(app)
 
 @app.route('/')
 def home():
     return jsonify({'text':'Hello World!'})
+
+@app.route("/CORS")
+def helloWorld():
+  return "Hello, cross-origin-world!"
 
 class Employees(Resource):
     def get(self):
