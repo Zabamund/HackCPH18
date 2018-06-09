7c0481a224f881587ba550f59ef5bb66999c7741
Zabamund <fracgeol@gmail.com>
2018-06-09 13:27:15 +0200

    update http service

--- hack18grv/grv-app/src/app/shared/http-service.service.ts
+++ hack18grv/grv-app/src/app/shared/http-service.service.ts
@@ -5,6 +5,22 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root'
 })
 export class HttpServiceService {
+  serverData: JSON;
+  employeeData: JSON;
 
-  constructor(private http: HttpClient) { }
+  constructor(private httpClient: HttpClient) { }
+
+  sayHi() {
+    this.httpClient.get('http://127.0.0.1:5002/').subscribe(data => {
+      this.serverData = data as JSON;
+      console.log(this.serverData);
+    })
+  }
+
+  getAllEmployees() {
+    this.httpClient.get('http://127.0.0.1:5002/employees').subscribe(data => {
+      this.employeeData = data as JSON;
+      console.log(this.employeeData);
+    })
+  }
 }
