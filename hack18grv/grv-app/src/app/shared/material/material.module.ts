e5e1f8837e91d908347fdc6836c7b47c8bbdfc9b
Zabamund <fracgeol@gmail.com>
2018-06-09 10:46:24 +0200

    setting up the views

--- hack18grv/grv-app/src/app/shared/material/material.module.ts
+++ hack18grv/grv-app/src/app/shared/material/material.module.ts
@@ -2,16 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { MatGridListModule } from '@angular/material/grid-list';
-
+import { MatSliderModule } from '@angular/material/slider';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
   imports: [
     CommonModule,
-    MatGridListModule
+    MatGridListModule,
+    MatSliderModule,
+    MatCardModule
   ],
   declarations: [],
   exports: [
-    MatGridListModule
+    MatGridListModule,
+    MatSliderModule,
+    MatCardModule
   ]
 })
 export class MaterialModule { }
